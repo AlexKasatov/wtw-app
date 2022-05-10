496adc9c86dde61a39f64d881ee31df1681974e1
Alex Morton <alexmorton_dev@protonmail.com>
2022-05-10 19:12:23 +0300

    improve pagination with additional sections

--- src/components/UI/spinner/Pagination/Pagination.jsx
+++ src/components/UI/spinner/Pagination/Pagination.jsx
@@ -1,20 +1,64 @@
+/* eslint-disable prefer-const */
 /* eslint-disable no-plusplus */
-import { Children, useMemo } from 'react';
+import { Children, useMemo, useState, useEffect } from 'react';
 import { ActiveButton, PaginationList } from './Pagination.styled';
 import { Button } from '../Button';
 
 const Pagination = ({ currentPage, onCurrentPage, totalCountries, countriesPerPage, setCurrentPage }) => {
-        const paginationLinks = [];
+        const [arrayCurrentButton, setArrayCurrentButton] = useState([]);
+        const numberOfPaginationLinks = [];
+
+        // arrayCurrentButton === numberOfPaginationLinks in old code
 
         useMemo(() => {
                 for (let i = 1; i <= Math.ceil(totalCountries / countriesPerPage); i++) {
-                        paginationLinks.push(i);
+                        numberOfPaginationLinks.push(i);
                 }
                 // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, [totalCountries, countriesPerPage, onCurrentPage]);
+        }, [totalCountries, countriesPerPage, onCurrentPage, arrayCurrentButton]);
+
+        useEffect(() => {
+                let tempNumberOfPaginationLinks = [...arrayCurrentButton];
+
+                let dotInitial = '...';
+                let dotLeft = '...';
+                let dotRight = '...';
+
+                if (currentPage >= 1 && currentPage <= 3) {
+                        tempNumberOfPaginationLinks = [1, 2, 3, 4, dotInitial, numberOfPaginationLinks.length];
+                } else if (currentPage === 4) {
+                        const sliced = numberOfPaginationLinks.slice(0, 5);
+                        tempNumberOfPaginationLinks = [...sliced, dotInitial, numberOfPaginationLinks.length];
+                } else if (currentPage > 4 && currentPage < numberOfPaginationLinks.length - 2) {
+                        const sliced1 = numberOfPaginationLinks.slice(currentPage - 2, currentPage);
+                        const sliced2 = numberOfPaginationLinks.slice(currentPage, currentPage + 1);
+                        tempNumberOfPaginationLinks = [
+                                1,
+                                dotLeft,
+                                ...sliced1,
+                                ...sliced2,
+                                dotRight,
+                                numberOfPaginationLinks.length,
+                        ];
+                } else if (currentPage > numberOfPaginationLinks.length - 3) {
+                        const sliced = numberOfPaginationLinks.slice(numberOfPaginationLinks.length - 4);
+                        tempNumberOfPaginationLinks = [1, dotLeft, ...sliced];
+                } else if (currentPage === dotInitial) {
+                        setCurrentPage(arrayCurrentButton[arrayCurrentButton.length - 3] + 1);
+                } else if (currentPage === dotRight) {
+                        setCurrentPage(arrayCurrentButton[3] + 2);
+                } else if (currentPage === dotLeft) {
+                        setCurrentPage(arrayCurrentButton[3] - 2);
+                }
+
+                setArrayCurrentButton(tempNumberOfPaginationLinks);
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, [currentPage]);
 
         const handleCurrentPageIncrement = () => {
-                if (currentPage < paginationLinks.length) setCurrentPage((prev) => prev + 1);
+                if (currentPage < numberOfPaginationLinks.length) {
+                        setCurrentPage((prev) => prev + 1);
+                }
         };
 
         const handleCurrentPageDecrement = () => {
@@ -28,7 +72,7 @@ const Pagination = ({ currentPage, onCurrentPage, totalCountries, countriesPerPa
                                         Prev
                                 </Button>
                                 {Children.toArray(
-                                        paginationLinks.map((number) => (
+                                        arrayCurrentButton.map((number) => (
                                                 <li>
                                                         {currentPage === number ? (
                                                                 <ActiveButton
@@ -49,8 +93,8 @@ const Pagination = ({ currentPage, onCurrentPage, totalCountries, countriesPerPa
                                         ))
                                 )}
                                 <Button
-                                        disabled={currentPage === paginationLinks.length}
                                         onClick={handleCurrentPageIncrement}
+                                        disabled={currentPage === numberOfPaginationLinks.length}
                                 >
                                         Next
                                 </Button>
